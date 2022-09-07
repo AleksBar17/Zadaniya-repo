5fc5a4dfaeb9bf18fe224acfcbb62ab33a45f26d
Aleksandra <alez-brize@mail.ru>
2022-09-07 20:23:50 +0300

    DZ

---
+++ zadanie/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99)
+{
+    int i = num;
+    while(i > 999) 
+    {
+        i/=10;
+    }
+
+Console.WriteLine(i%10);
+}
+else
+Console.WriteLine("Третьей цифры нет");

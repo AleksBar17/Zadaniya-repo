d9b3d6d82a95f19bf0d7ff9f5ebc593e73891c81
Aleksandra <alez-brize@mail.ru>
2022-09-10 23:50:39 +0300

    таблица кубов чисел

---
+++ cubnumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine ("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= num)
+{
+    Console.WriteLine($"{Math.Pow(index, 3)}");
+
+index = index + 1;
+}
